b8efa1556e76e94ee7e60adff8421206f81398cf
Urgen Sherpa <urgen444@gmail.com>
2021-05-20 13:50:07 +0545

    port number changed

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const hbs = require("hbs");
 const app = express();
 
 // Define paths for express config
-const port = 3000;
+const port = process.env.PORT || 3000;
 const publicDir = path.join(__dirname, "../public");
 const viewsDir = path.join(__dirname, "../templates/views");
 const partialDir = path.join(__dirname, "../templates/partials");
